2c38e3a53ace915ee289acb6f149212e3bdf735d
assia <abf70@alu.ua.es>
2026-02-18 12:26:52 +0100

    main

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 # main.py
-
 from persona import Persona
+import random
+
+vocales = random.choice(['A','E','I','O','U'])
+numeros = random.randint(11111111,99999999)
+dni = str(numeros)+vocales
 from tarjeta_premium import Tarjeta
 
 if __name__ == '__main__':
---
+++ prueba.py
@@ -0,0 +1,7 @@
+import random
+vocales = random.choice(['A','E','I','O','U'])
+numeros = random.randint(11111111,99999999)
+dni = str(numeros)+vocales
+
+
+print(codigo)
