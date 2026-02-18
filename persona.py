c539e3eda819f2366a4bf2eb1ac90758c8ff26e6
tamara-max <tbb20@alu.ua.es>
2026-02-18 12:00:10 +0100

    clase persona no terminada, falta tarjeta

---
+++ persona.py
@@ -0,0 +1,16 @@
+# persona.py
+
+class Persona:
+    def __init__(self, dni, nombre, apellido, tarjeta_premium = None):
+        self.dni = dni
+        self.nombre = nombre
+        self.apellido = apellido
+        self.tarjeta_premium = tarjeta_premium
+
+    def __str__(self):
+        cadena = f'Persona {self.nombre} {self.apellido} -DNI: {self.dni}'
+
+        if self.tarjeta_premium:
+            cadena += f' -Tarjeta: {self.tarjeta_premium}'
+
+        return cadena
