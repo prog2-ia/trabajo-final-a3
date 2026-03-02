245403bdb24d98d07e5c42805c39083423670095
tamara-max <tbb20@alu.ua.es>
2026-03-02 14:11:54 +0100

    herencia de clase padre Producto y clases hijas ProductoCoche y ProductoRopa

--- producto_coche.py
+++ producto_coche.py
@@ -1,7 +1,8 @@
 # producto_coche.py
 
 class Coche:
-    def __init__(self, matricula, marca, ano, combustible, kilometros_recorridos):
+    def __init__(self, id, titulo, precio, vendedor, estado, stock, fecha_publicacion, matricula, marca, ano, combustible, kilometros_recorridos):
+        super().__init__(id, titulo, precio, vendedor, estado, stock, fecha_publicacion)
         self.matricula = matricula
         self.marca = marca
         self.kilometros_recorridos = kilometros_recorridos
