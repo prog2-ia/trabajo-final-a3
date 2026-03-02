7e3210d75f0cdfdebc71b5e299d90d784211f7a3
assia <abf70@alu.ua.es>
2026-03-02 14:12:02 +0100

    herencia de clase padre Producto y clase hija ProductoElectronico

--- producto_electronico.py
+++ producto_electronico.py
@@ -1,7 +1,8 @@
 # producto_electrónico.py
 
 class ProductoElectronico:
-    def __init__(self, marca, modelo, garantia):
+    def __init__(self, id, titulo, precio, vendedor, estado, stock, fecha_publicacion, marca, modelo, garantia):
+        super().__init__(id, titulo, precio, vendedor, estado, stock, fecha_publicacion)
         self.modelo = modelo
         self.marca = marca
         self.garantia = garantia
